1ee3f063b4b39a16aa59ef248ceea642f7753072
aamir <aamirchanna127@gmail.com>
2025-02-03 06:49:29 -0800

    set error

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,5 +1,5 @@
 import SectionHeading from "@/app/Helper/SectionHeading";
-import { aboutInfo, BaseInfo } from "@/Data/data";
+import {  BaseInfo } from "@/Data/data";
 import { FaCheck } from "react-icons/fa";
 import Image from "next/image";
 
